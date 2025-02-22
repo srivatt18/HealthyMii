27b9c1e85240a9a1ca7b84daa2f39c675964cc6a
srivatt18 <srivattikuti90@gmail.com>
2025-02-22 17:26:28 -0600

    Initial Commit

---
+++ HealthyMii/HealthyMiiApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthyMiiApp.swift
+//  HealthyMii
+//
+//  Created by Sri Vattikuti on 2/22/25.
+//
+
+import SwiftUI
+
+@main
+struct HealthyMiiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HealthyMiiTests/HealthyMiiTests.swift
@@ -0,0 +1,16 @@
+//
+//  HealthyMiiTests.swift
+//  HealthyMiiTests
+//
+//  Created by Sri Vattikuti on 2/22/25.
+//
+
+import Testing
+
+struct HealthyMiiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
